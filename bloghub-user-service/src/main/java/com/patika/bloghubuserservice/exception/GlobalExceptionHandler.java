7d4e6ad49ee6a5e09f76d2c7aec06b6344e39504
melihbesel <mbesel2005@gmail.com>
2024-08-11 20:03:22 +0300

    Added Kafka producer implementation to bloghub-user-service.

--- bloghub-user-service/src/main/java/com/patika/bloghubuserservice/exception/GlobalExceptionHandler.java
+++ bloghub-user-service/src/main/java/com/patika/bloghubuserservice/exception/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package com.patika.bloghubuserservice.exception;
 
 import com.patika.bloghubuserservice.dto.response.GenericResponse;
+import com.patika.bloghubuserservice.producer.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RequiredArgsConstructor
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final KafkaProducer kafkaProducer;
+
     @ExceptionHandler(BlogHubException.class)
     public GenericResponse handleBlogHubException(BlogHubException blogHubException){
+        log.info("ExceptionHandler çağrıldı");
+        kafkaProducer.sendException(blogHubException.getMessage());
         return GenericResponse.failed(blogHubException.getMessage());
     }
 
