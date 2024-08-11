0992c1c1fc0ac9131904ca9b3f75e529041bd5f5
melihbesel <mbesel2005@gmail.com>
2024-08-11 22:44:25 +0300

    Added Kafka producer implementation and Runtime ExceptionHandler to bloghub-payment-service.

--- bloghub-payment-service/src/main/java/com/patika/bloghubpaymentservice/exception/GlobalExceptionHandler.java
+++ bloghub-payment-service/src/main/java/com/patika/bloghubpaymentservice/exception/GlobalExceptionHandler.java
@@ -1,15 +1,31 @@
 package com.patika.bloghubpaymentservice.exception;
 
 import com.patika.bloghubpaymentservice.dto.response.GenericResponse;
+import com.patika.bloghubpaymentservice.producer.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RequiredArgsConstructor
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final KafkaProducer kafkaProducer;
+
     @ExceptionHandler(BlogHubPaymentException.class)
     public GenericResponse handleBlogHubPaymentException(BlogHubPaymentException blogHubPaymentException){
+        log.info("BlogHubPaymentException ExceptionHandler çağrıldı");
+        kafkaProducer.sendException(blogHubPaymentException.getMessage());
         return GenericResponse.failed(blogHubPaymentException.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleException(RuntimeException exception){
+        log.info("RuntimeException ExceptionHandler çağrıldı");
+        kafkaProducer.sendException(exception.getMessage());
+        return exception.getMessage();
     }
 
 }
---
+++ bloghub-payment-service/src/main/java/com/patika/bloghubpaymentservice/producer/KafkaProducer.java
@@ -0,0 +1,21 @@
+package com.patika.bloghubpaymentservice.producer;
+
+import com.patika.bloghubpaymentservice.producer.constant.KafkaConstants;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public final class KafkaProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendException(String message) {
+        kafkaTemplate.send(KafkaConstants.BLOG_INDEX_TOPICS, message);
+        log.info("exception kafka'ya gönderildi. topics:{}, message: {}", KafkaConstants.BLOG_INDEX_TOPICS, message);
+    }
+
+}
